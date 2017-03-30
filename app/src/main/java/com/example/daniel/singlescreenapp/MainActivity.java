ad2dc55fad276836255f26df6f36d78bf7be546e
sansst <sansst@gmail.com>
2017-03-30 15:43:53 +0200

    First version

--- app/src/main/java/com/example/daniel/singlescreenapp/MainActivity.java
+++ app/src/main/java/com/example/daniel/singlescreenapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.daniel.singlescreenapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void ButtonPhone(View view) {
+
+    }
+
+    public void ButtonMail(View view) {
+
+    }
+
+    public void ButtonMaps(View view) {
+
     }
 }
