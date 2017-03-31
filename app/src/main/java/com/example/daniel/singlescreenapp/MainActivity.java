0866a0e8b5cb61adfa7cb1d5eb2d70db26f4ad2a
sansst <sansst@gmail.com>
2017-03-31 14:49:56 +0200

    First version

--- app/src/main/java/com/example/daniel/singlescreenapp/MainActivity.java
+++ app/src/main/java/com/example/daniel/singlescreenapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.daniel.singlescreenapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,14 +15,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void ButtonPhone(View view) {
-
+        String phone = "+34666777888";
+        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phone, null));
+        startActivity(intent);
     }
 
     public void ButtonMail(View view) {
+        String email = "inbox@danielsbike.es";
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:" + email));
+        startActivity(Intent.createChooser(emailIntent, "Chooser Title"));
 
     }
 
     public void ButtonMaps(View view) {
+        Uri gmmIntentUri = Uri.parse("geo:41.2237699,1.7216385");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mapIntent);
 
     }
 }
